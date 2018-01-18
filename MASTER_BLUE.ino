0087f67efa3677eb393d7ea1a7167c9ca6073ae4
Joseph Katigbak <34512892+JoeKatigbak@users.noreply.github.com>
2018-01-18 23:10:53 +0800

    Add files via upload

---
+++ MASTER_BLUE.ino
@@ -0,0 +1,32 @@
+/*Created by Joseph Locsin Katigbak, LX Robotics Engineer, 01/18/18
+ * Master Bluetooth code which reads slave data to control a digital output(led).
+*/
+
+
+int ledPin = 13;
+int state = 0;
+
+void setup() {
+pinMode(ledPin, OUTPUT);
+digitalWrite(ledPin, LOW);
+Serial.begin(9600); // Default communication rate of the Bluetooth module
+}
+
+void loop() {
+ if(Serial.available() > 0){ // Checks whether data is comming from the serial port
+    state = Serial.read(); // Reads the data from the serial port
+ }
+ 
+ // Data is read from the slave and corresponds to the following output
+ 
+ if (state == '1') 
+ {
+  digitalWrite(ledPin, HIGH); // LED ON
+  state = 0;
+ }
+ else if (state == '0')
+ {
+  digitalWrite(ledPin, LOW); // LED OFF
+  state = 0;
+ }
+}
