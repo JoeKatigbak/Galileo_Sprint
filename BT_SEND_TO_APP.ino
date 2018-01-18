f98a4059433260577c2e20004025092623cc5d28
Joseph Katigbak <34512892+JoeKatigbak@users.noreply.github.com>
2018-01-19 00:45:22 +0800

    Add files via upload

---
+++ BT_SEND_TO_APP.ino
@@ -0,0 +1,21 @@
+/*Created by Joseph Locsin Katigbak, LX Robotics Engineer, 01/18/18
+ * Simple BT function test
+ * - as MCU ISBAT send data to Mobile app through bluetooth.
+*/
+
+int pwm = A0;
+
+void setup() {
+Serial.begin(9600);
+pinmode(pwm, INPUT);
+}
+
+void loop() {
+
+float val = analogRead(pwm);
+
+float volts = val/205; //dividing by 205 directly
+
+Serial.print(volts); //sends text 
+delay(100);
+}
